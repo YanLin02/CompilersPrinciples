759644769177e8531c2475dfcab6b70e629a1f80
Sun Yanlin <1310400098@qq.com>
2023-11-03 19:53:55 +0800

    词法设定添加

---
+++ Lexical analyzer/main.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+/*
+* letter_ : [a-zA-Z_];
+* digit : [0-9];
+* 
+* identifier : letter_ (letter_ | digit)*;
+* 
+* keyword:
+* auto, bool, break, case, char, const, constexpr, continue, default, 
+* do, double,else, enum, extern, false, float, for, goto, if, inline, 
+* int, long, namespace, nullptr,register, restrict, return, short, signed, 
+* sizeof, static, static_assert, struct,switch, true, typedef, typeof, 
+* union, unsigned, using, void, volatile, while.
+* 
+* boolean-literal: true, false;
+* pointer-literal: nullptr;
+* character-literal
+* string-literal
+* integer_literal : digit+
+* floating_literal : digit* '.' digit+ ([eE] [+-]? digit+)?
+* 
+* op_or_punc : '{'|'}'|'['|']'|'('|')'|';'|':'|'.'|'+'|'-'|'*'|'/'|'%'|'^'|'&'|'|'|'~'|'!'|'='|'<'|'>'|'=='|'!='|'<='|'>='|'&&'|'||'|',';
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+
+
+
+	system("pause");
+	return 0;
+}
